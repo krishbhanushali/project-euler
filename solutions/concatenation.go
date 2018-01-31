77a0d417462664e9dfb422c5cf604500efce8992
Krish Bhanushali <kbhanushali@ini.usc.edu>
2018-01-30 16:02:43 -0800

    pythagorean triplet

---
+++ solutions/concatenation.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+"fmt"
+"strings"
+)
+
+func main (){
+	concatenation:= strings.Join([]string{"a","b","c"},"")
+	fmt.Println(concatenation)
+}
---
+++ solutions/special_pythagorean_triplet.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	for a := 1; a < 1000; a++ {
+		for b := a + 1; b < 1000; b++ {
+			for c := b + 1; c < 1000; c++ {
+				if a*a+b*b == c*c && a+b+c == 1000 {
+					fmt.Println(a * b * c)
+					break
+				}
+			}
+		}
+	}
+}
+
+// a = m^2 - n^2, b=2*m*n, c = m^2 + n^2
